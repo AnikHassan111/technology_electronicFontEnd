32f11e9f75c48bfcb8391fd320bdd93989e6e79e
AnikHassan111 <miaanik4@gmail.com>
2023-10-21 22:57:28 +0600

    recheck complate

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             .then(data => setBrandData(data))
     }, [])
     return (
-        <div className="max-w-7xl mx-auto mt-10">
+        <div className="max-w-7xl mx-auto mt-12">
             
             {
                 brandData.map(brand => <AboutCard key={brand._id} data={brand}></AboutCard>)
